3da10388ee28aa8c6cf5111ca8981f4e1631f65e
Jakub Kawka <jakubkawka2005@gmail.com>
2023-10-20 13:52:59 +0200

    create a sql file with init users table

---
+++ docker/dump.sql
@@ -0,0 +1,12 @@
+CREATE TABLE IF NOT EXISTS public.users (
+    id SERIAL PRIMARY KEY,
+    email VARCHAR(100) NOT NULL,
+    firstname VARCHAR(50) NOT NULL,
+    lastname VARCHAR(50) NOT NULL,
+    password VARCHAR(100) NOT NULL
+);
+
+INSERT INTO public.users VALUES 
+(default, 'j.kawka@zsp9.elodz.edu.pl', 'Jakub', 'Kawka', 'zaq1@WSX'),
+(default, 'a.kowalski@zsp9.elodz.edu.pl', 'Adam', 'Kowalski', 'zaq1@WSX'),
+(default, 'n.kwiatkowska@zsp9.elodz.edu.pl', 'Natalia', 'Kwiatkowska', 'zaq1@WSX');
