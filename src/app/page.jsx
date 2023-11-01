92709725894632f661d3728f3b1259ed9a07cd70
Jakub Kawka <jakubkawka2005@gmail.com>
2023-11-01 17:52:08 +0100

    add new post to database

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -3,10 +3,13 @@ import { Pool } from "pg";
 import { loginCheck } from "./api/login";
 import getUser from "./api/users/get";
 import getAllPosts from "./api/posts/getAll";
-import timeAgo from "./utils/time";
 
 import FormNewPost from "@components/FormNewPost";
+import Posts from "@components/Posts";
 
+export const metadata = {
+  title: "schcool | główna",
+};
 
 export default async function HomePage() {
   await loginCheck(false);
@@ -17,18 +20,7 @@ export default async function HomePage() {
   return (
     <div>
       <FormNewPost user={user}/>
-      {posts.map((post) => (
-        <div key={post.id}>
-          <p>
-            <b>
-              {post.firstname.toLowerCase()} {post.lastname.toLowerCase()}
-            </b>{" "}
-            {post.email.split("@")[0]} · {timeAgo(post.create_date.getTime())}
-          </p>
-          <p>{post.content}</p>
-          <br />
-        </div>
-      ))}
+      <Posts posts={posts} />
     </div>
   );
 }
