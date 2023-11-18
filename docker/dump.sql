49399824913a77608e8d81478bdd2675863029b3
Jakub Kawka <jakubkawka2005@gmail.com>
2023-11-18 10:10:38 +0100

    create likes table in a database and insert some data to them

--- docker/dump.sql
+++ docker/dump.sql
@@ -6,11 +6,6 @@ CREATE TABLE IF NOT EXISTS public.users (
     password VARCHAR(100) NOT NULL
 );
 
-INSERT INTO public.users VALUES 
-(DEFAULT, 'j.kawka@zsp9.elodz.edu.pl', 'Jakub', 'Kawka', 'zaq1@WSX'),
-(DEFAULT, 'a.kowalski@zsp9.elodz.edu.pl', 'Adam', 'Kowalski', 'zaq1@WSX'),
-(DEFAULT, 'n.kwiatkowska@zsp9.elodz.edu.pl', 'Natalia', 'Kwiatkowska', 'zaq1@WSX');
-
 CREATE TABLE IF NOT EXISTS public.users_devices (
     user_id INT REFERENCES public.users(id) NOT NULL,
     device_id VARCHAR(100) NOT NULL,
@@ -25,7 +20,26 @@ CREATE TABLE IF NOT EXISTS public.posts (
     anonymous BOOLEAN NOT NULL
 );
 
+CREATE TABLE IF NOT EXISTS public.likes (
+    id SERIAL PRIMARY KEY NOT NULL,
+    post_id INT REFERENCES public.posts(id) NOT NULL,
+    user_id INT REFERENCES public.users(id) NOT NULL,
+    date TIMESTAMP NOT NULL
+);
+
+
+INSERT INTO public.users VALUES 
+(DEFAULT, 'j.kawka@zsp9.elodz.edu.pl', 'Jakub', 'Kawka', 'zaq1@WSX'),
+(DEFAULT, 'a.kowalski@zsp9.elodz.edu.pl', 'Adam', 'Kowalski', 'zaq1@WSX'),
+(DEFAULT, 'n.kwiatkowska@zsp9.elodz.edu.pl', 'Natalia', 'Kwiatkowska', 'zaq1@WSX');
+
 INSERT INTO public.posts VALUES
 (DEFAULT, 1, 'Siemano widzowie', '2023-10-10 12:00:00', false),
 (DEFAULT, 2, 'xdddd', '2023-10-10 12:01:00', false),
 (DEFAULT, 1, 'aha', '2023-10-12 12:00:00', false);
+
+INSERT INTO public.likes VALUES
+(DEFAULT, 1, 3, '2023-10-12 12:00:00'),
+(DEFAULT, 1, 2, '2023-10-10 12:01:00'),
+(DEFAULT, 2, 1, '2023-10-12 12:00:00'),
+(DEFAULT, 2, 3, '2023-10-12 12:00:00');
