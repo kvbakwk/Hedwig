8fdc5e9be079f2ca915895435f9e7c7118d829d7
Jakub Kawka <jakubkawka2005@gmail.com>
2023-11-21 17:25:59 +0100

    handle saving post

--- src/app/api/posts/get.js
+++ src/app/api/posts/get.js
@@ -7,6 +7,8 @@ export default async function get() {
     "SELECT p.id, p.user_id, u.email, u.firstname, u.lastname, p.content, p.create_date, p.anonymous FROM public.post AS p JOIN public.user AS u ON p.user_id = u.id ORDER BY p.create_date DESC;";
   const queryLikes =
     "SELECT user_id FROM public.like_user_post WHERE post_id = $1;";
+  const querySaves =
+    "SELECT user_id FROM public.save_user_post WHERE post_id = $1;";
 
   const client = new Pool();
   const posts = (await client.query(queryPosts)).rows;
@@ -16,6 +18,13 @@ export default async function get() {
       posts[index] = {
         ...posts[index],
         likes: res.rows.map((like) => like.user_id),
+      };
+    }),
+    posts.map(async (post, index) => {
+      const res = await client.query(querySaves, [post.id]);
+      posts[index] = {
+        ...posts[index],
+        saves: res.rows.map((save) => save.user_id),
       };
     })
   );
--- src/components/posts/PostFooter.jsx
+++ src/components/posts/PostFooter.jsx
@@ -3,13 +3,16 @@
 import { useState, useEffect } from "react";
 
 import like from "@app/api/posts/like";
+import save from "@app/api/posts/save";
 
-export default function PostFooter({ user_id, post_id, likes }) {
+export default function PostFooter({ user_id, post_id, likes, saves }) {
   const [liked, setLiked] = useState(likes.find((id) => id === user_id));
+  const [saved, setSaved] = useState(saves.find((id) => id === user_id));
   const [likesCount, setLikesCount] = useState(likes.length);
 
   useEffect(() => {
     setLiked(likes.find((id) => id === user_id));
+    setSaved(saves.find((id) => id === user_id));
     setLikesCount(likes.length);
   }, [likes]);
 
@@ -18,6 +21,11 @@ export default function PostFooter({ user_id, post_id, likes }) {
       ? setLikesCount(likesCount + 1)
       : setLikesCount(likesCount - 1);
     setLiked(!liked);
+  };
+
+  const handleSave = async () => {
+    await save(user_id, post_id);
+    setSaved(!saved);
   };
 
   return (
@@ -31,8 +39,8 @@ export default function PostFooter({ user_id, post_id, likes }) {
         </span>
         {likesCount}
       </div>
-      <div className="cursor-pointer" onClick={() => {}}>
-        zapisz
+      <div className="cursor-pointer" onClick={handleSave}>
+        {saved ? "zapisany" : "zapisz"}
       </div>
     </div>
   );
