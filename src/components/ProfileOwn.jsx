746e1509426273a5be4af2b6845a47f657f1d243
Jakub Kawka <jakubkawka2005@gmail.com>
2023-11-25 16:14:32 +0100

    add users likes to profile site

--- src/components/ProfileOwn.jsx
+++ src/components/ProfileOwn.jsx
@@ -1,6 +1,7 @@
 import Link from "next/link";
 import Posts from "./posts/Posts";
-import getPosts from "@app/api/posts/user/get";
+import getPosts from "@app/api/posts/get";
+import getUserPosts from "@app/api/posts/user/get";
 
 export default async function ProfileOwn({ user, option }) {
   return (
@@ -15,12 +16,29 @@ export default async function ProfileOwn({ user, option }) {
         <Link className="cursor-pointer" href={`/profil/${user.id}/odpowiedzi`}>
           odpowiedzi
         </Link>
+        <Link className="cursor-pointer" href={`/profil/${user.id}/polubione`}>
+          polubione
+        </Link>
       </div>
       {(option === "posty" || option === undefined) && (
-        <Posts user={user} posts={await getPosts(user.id, true, false, true)} />
+        <Posts
+          user={user}
+          posts={await getUserPosts(user.id, true, false, true)}
+        />
       )}
       {option === "odpowiedzi" && (
-        <Posts user={user} posts={await getPosts(user.id, false, true, true)} />
+        <Posts
+          user={user}
+          posts={await getUserPosts(user.id, false, true, true)}
+        />
+      )}
+      {option === "polubione" && (
+        <Posts
+          user={user}
+          posts={(await getPosts(user.id)).filter((post) =>
+            post.likes.find((id) => id === user.id)
+          )}
+        />
       )}
     </div>
   );
